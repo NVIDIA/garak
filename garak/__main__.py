6ffabb016782b497116070e9273645d59e62633f
iamnotcj <69222710+iamnotcj@users.noreply.github.com>
2025-08-06 16:42:26 -0500

    Garak API

--- garak/__main__.py
+++ garak/__main__.py
@@ -3,10 +3,14 @@
 import sys
 
 from garak import cli
+from garak import api
 
 
 def main():
-    cli.main(sys.argv[1:])
+    if "--remote" in sys.argv:
+        api.run()
+    else:
+        cli.main(sys.argv[1:])
 
 
 if __name__ == "__main__":
---
+++ garak/api.py
@@ -0,0 +1,36 @@
+from garak.cli import main
+from socket import socket, AF_INET, SOCK_STREAM
+import sys
+
+# 2 sockets, one that receives the arguments, and another that is dedicated to sending the output as a client.
+
+ADDRESS = "localhost"
+PORT = 45345
+
+
+def run():
+    garak_socket = socket(AF_INET, SOCK_STREAM)
+    garak_socket.bind((ADDRESS, PORT))
+    garak_socket.listen()
+    # Client needs to tell the api which port to send the output to.
+    while True:
+        client_socket, client = garak_socket.accept()
+
+        arguments = client_socket.recv(1024).decode()
+        arguments = arguments.split()
+
+        client_address = client[0]
+        client_port = int(arguments[0])
+
+        # This just makes sure that the output of the cli is returned to the client.
+        def redirectOut(port=0, host=0):
+            if port == 0 or host == 0:
+                raise ValueError("Client port or address weren't found")
+            sock = socket(AF_INET, SOCK_STREAM)
+            sock.connect((host, port))
+            with sock.makefile("w") as file:
+                sys.stdout = file
+                main(arguments[1:])
+            return sock  # if caller needs to access it raw
+
+        redirectOut(client_port, client_address)
