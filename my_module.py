e5d0611f6cb25009455cf5bf31d6d8386a80155f
noah <noah.oeksuez@cyphersec.eu>
2025-10-28 19:04:12 +0000

    cleanup

--- my_module.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def function_name(prompt: str) -> list[str]:
-
-    json_string = '{"test_key": "test_value", "test_key_2": [1, 2, 3]}'
-    json.loads(json_string) # check that the string is valid json
-
-    return [json_string]
--- test.py
+++
@@ -1,5 +0,0 @@
-import garak
-import garak.cli
-import my_module
-
-garak.cli.main("--target_type function --target_name my_module#function_name --probes goodside.ThreatenJSON".split())
